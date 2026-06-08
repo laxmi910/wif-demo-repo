8d51719502515ea9b881aa7ab8e8c6979b0beff1
laxmi910 <54587932+laxmi910@users.noreply.github.com>
2026-06-08 12:11:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "bktjgjfjyfjh09"
+  name          = "bkjshkjskh09"
   location      = "US-CENTRAL1"
   force_destroy = true
 
@@ -7,7 +7,7 @@ resource "google_storage_bucket" "my-bucket" {
 }
 
 resource "google_storage_bucket" "my-bucket2" {
-  name          = "bktjgkjlklkj0"
+  name          = "bkjgbkjshglkh"
   location      = "US-CENTRAL1"
   force_destroy = true
 
